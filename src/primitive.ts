6e6ba5c6030593d76f5897c21b1b3fd82cbd28f2
Sara-Rubaya <sararubaya4800@gmail.com>
2026-05-04 14:52:58 +0600

    adding typescript repo

---
+++ src/primitive.ts
@@ -0,0 +1,15 @@
+// JS - TS
+// string, number, boolean, undefined, null
+
+// TS: never, unknown, void
+
+let userName: string = "Sara123";
+let userId = 123;
+
+
+console.log(userId.toFixed(2)); // "123.00"
+
+let isAdmin: boolean = false;
+isAdmin = true;
+
+let x: undefined = undefined;
